f3cb2f7d41bacc254b3d53b95dd14161c81ee9a3
marcomoreschi <marcomoreschi91@hotmail.it>
2020-09-22 10:12:15 +0000

    added order history

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,10 +14,10 @@ class OrderAdmin(admin.ModelAdmin):
                        'delivery_cost', 'order_total',
                        'grand_total',)
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'delivery_cost',
+              'county', 'delivery_cost',
               'order_total', 'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
